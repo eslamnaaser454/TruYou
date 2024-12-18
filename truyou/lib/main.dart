a7ce16a1286836c0d63e555855f5c787b41f06cc
Omar-lashin <omaramrabdelkaderlashin@gmail.com>
2024-12-18 23:07:25 +0200

    the welcome pages front and the media folder

--- truyou/lib/main.dart
+++ truyou/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 // import 'package:truyou/screens/Sign_in.dart';
 import 'package:truyou/Login-Sginup/Sign_up.dart';
+import 'package:truyou/welcomeScreens/welcomescreen.dart';
+import 'package:truyou/welcomeScreens/welcomescreenCBT.dart';
+import 'package:truyou/welcomeScreens/welcomescreenChatBot.dart';
+import 'package:truyou/welcomeScreens/welcomescreenExplore.dart';
+import 'package:truyou/welcomeScreens/welcomescreenSelf_assesment.dart';
 
 void main() {
-  runApp(TruYouApp());
+  runApp(const TruYouApp());
 }
 
 class TruYouApp extends StatelessWidget {
@@ -11,6 +16,15 @@ class TruYouApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SignUp();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const WelcomeScreen(),
+          '/cbt': (context) => const Welcomescreencbt(),
+          '/chatbot': (context) => const Welcomescreenchatbot(),
+          '/explore': (context) => const Welcomescreenexplore(),
+          '/self_assessment': (context) => const WelcomescreenselfAssesment(),
+        });
   }
 }
